7b68e5129b5df7b11b38a90836a11605892ebaca
tranvietanh0 <tranvietanh0507@gmail.com>
2026-02-06 13:44:12 +0700

    Add Local Data Editor and serialization configuration for user data management

---
+++ Editor/LocalData/LocalDataEditor.cs
@@ -0,0 +1,106 @@
+﻿namespace Submodules.UITemplate.Editor.LocalData
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using GameFoundationCore.Scripts.DI;
+    using GameFoundationCore.Scripts.Models.Interfaces;
+    using GameFoundationCore.Scripts.Utilities.UserData;
+    using Newtonsoft.Json;
+    using Sirenix.OdinInspector;
+    using Sirenix.OdinInspector.Editor;
+    using Sirenix.Serialization;
+    using UniT.Extensions;
+    using UnityEditor;
+    using UnityEngine;
+
+    public class LocalDataEditor : OdinEditorWindow
+    {
+        private const string LocalDataPrefix = BaseHandleUserDataServices.UserDataPrefix;
+
+        private static readonly JsonSerializerSettings JsonSetting = BaseHandleUserDataServices.JsonSetting;
+
+        [OdinSerialize]
+        [HideLabel]
+        [ListDrawerSettings(ShowFoldout = false, ShowPaging = true, ShowItemCount = true, IsReadOnly = true, DraggableItems = false, NumberOfItemsPerPage = 5)]
+        private List<ILocalData> localData;
+
+        private Vector2 scrollPosition;
+
+        [Button(ButtonSizes.Medium)]
+        [GUIColor(0, 0.5f, 0)]
+        private void SyncWithRuntime()
+        {
+            if (!Application.isPlaying)
+            {
+                Debug.LogError("Sync with runtime only work in play mode");
+                this.localData = null;
+                return;
+            }
+
+            var handleUserDataServices = this.GetCurrentContainer().Resolve<IHandleUserDataServices>();
+            var cacheFieldInfo = handleUserDataServices.GetType()
+                .GetField("localDataCaches", BindingFlags.Instance | BindingFlags.NonPublic);
+            var localDataCaches = cacheFieldInfo?.GetValue(handleUserDataServices) as Dictionary<string, object>;
+            this.localData = localDataCaches?.Select(e => e.Value).Cast<ILocalData>().ToList();
+        }
+
+        [HorizontalGroup]
+        [Button(ButtonSizes.Large)]
+        private void LoadLocalData()
+        {
+            this.localData = LoadAllLocalData();
+            Debug.LogError($"Load Complete");
+        }
+
+        [HorizontalGroup]
+        [Button(ButtonSizes.Large)]
+        private void SaveLocalData()
+        {
+            if (this.localData is { Count: 0 }) return;
+
+            foreach (var data in this.localData)
+            {
+                var key  = $"{LocalDataPrefix}{data.GetType().Name}";
+                var json = JsonConvert.SerializeObject(data, JsonSetting);
+                PlayerPrefs.SetString(key, json);
+            }
+
+            Debug.LogError($"Save Complete");
+        }
+
+        [Button(ButtonSizes.Large)]
+        [GUIColor(0.5f, 0, 0)]
+        private void ClearLocalData()
+        {
+            PlayerPrefs.DeleteAll();
+            this.localData = null;
+            Debug.LogError($"Clear Complete");
+        }
+
+        [MenuItem("Vanh/Local Data Editor")]
+        public static void ShowWindow()
+        {
+            GetWindow(typeof(LocalDataEditor));
+        }
+
+        private static List<ILocalData> LoadAllLocalData()
+        {
+            var result = new List<ILocalData>();
+
+            var localDataTypes = typeof(ILocalData).GetDerivedTypes();
+
+            foreach (var type in localDataTypes)
+            {
+                var key = $"{LocalDataPrefix}{type.Name}";
+
+                if (!PlayerPrefs.HasKey(key)) continue;
+                var json = PlayerPrefs.GetString(key);
+
+                if (JsonConvert.DeserializeObject(json, type, JsonSetting) is ILocalData data) result.Add(data);
+            }
+
+            return result;
+        }
+    }
+}
