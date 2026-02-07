2b4b60e9fc196532d9fd868bb5fdc8912c40bbb2
tranvietanh0 <tranvietanh0507@gmail.com>
2026-02-07 14:40:23 +0700

    Add UITemplateLocalData class and associated metadata for local data management

---
+++ Scripts/Models/Core/Element/UITemplateLocalData.cs
@@ -0,0 +1,21 @@
+﻿using IUITemplateLocalData = UITemplate.Scripts.Models.Core.Interface.IUITemplateLocalData;
+
+namespace Submodules.UITemplate.Scripts.Models.Core.Element
+{
+    using System;
+    using GameFoundationCore.Scripts.Models.Interfaces;
+
+    public abstract class UITemplateLocalData<TController> : ILocalData, IUITemplateLocalData
+    {
+        Type IUITemplateLocalData.ControllerType => typeof(TController);
+
+        void ILocalData.Init()
+        {
+            this.Init();
+        }
+
+        protected virtual void Init()
+        {
+        }
+    }
+}
